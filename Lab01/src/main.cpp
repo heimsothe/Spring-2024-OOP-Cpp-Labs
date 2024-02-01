bb07b441ab7e31142cf360967b19ac3e21405dee
slothy49 <73962789+slothy49@users.noreply.github.com>
2024-02-01 16:23:04 +0000

    [Lab01] Started work on main.cpp. Still incomplete

--- Lab01/src/main.cpp
+++ Lab01/src/main.cpp
@@ -1,3 +1,10 @@
 //TODO - Add the Program Assignment boilerplate comment block here. You can find its template in the C++ Code Style Guide
 
+#include <iostream>
+#include "functions.h"
+
 // TODO - Build a main function for testing your code.
+
+int main(){
+    
+}
