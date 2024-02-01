8efe954a1fb4b2d00441c77b114bf97f11b75eca
slothy49 <73962789+slothy49@users.noreply.github.com>
2024-02-01 18:27:28 +0000

    [Lab01] Completed code for main.cpp. Program compiles and runs as expected. Next is to add headers and relevant comments.

--- Lab01/src/main.cpp
+++ Lab01/src/main.cpp
@@ -6,5 +6,20 @@
 // TODO - Build a main function for testing your code.
 
 int main(){
-    
+    int lakeArea = calculateLakeArea();
+    float lakeVolume = calculateLakeVolume(lakeArea);
+    int fishStock = calculateFishStock(lakeVolume);
+    int maxLicenses = calculateMaxLicenses(fishStock);
+
+    std::cout << "The area of the lake is: " << lakeArea << std::endl;
+
+    std::cout << "The volume of the lake is: " << lakeVolume << std::endl;
+
+    std::cout << "The lake will be stocked with: " << fishStock << " fish." << std::endl;
+
+    std::cout << "There will be a maximum of: " << maxLicenses << " licenses issued." << std::endl;
+
+    return 0;
+
+
 }
