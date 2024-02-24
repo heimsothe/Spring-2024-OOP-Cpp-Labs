d17985a0f2254bd54ef394ccc44b45b2e4c99c7d
slothy49 <73962789+slothy49@users.noreply.github.com>
2024-02-24 05:21:59 +0000

    [Lab03] Final comments for Student.cpp

--- Lab03/src/Student.cpp
+++ Lab03/src/Student.cpp
@@ -16,11 +16,12 @@ using std::setfill;
 using std::left;
 using std::right;
 
-
+//Empty Constructor
 Student::Student(){
 
 }
 
+//Full parameter Constructor
 Student::Student(int aSID, std::string aFName, std::string aLName, std::string anAddress, long aPhone, std::string aGrade, int aCount){
     setSID(aSID);
     setFName(aFName);
@@ -28,15 +29,13 @@ Student::Student(int aSID, std::string aFName, std::string aLName, std::string a
     setAddress(anAddress);
     setPhone(aPhone);
 
-    //we have a string of grades "80 70 60"
+    //sets object grades
+    //streams grades from aGrade into addGrade() which validates grades and adds to object
     std::istringstream gradeStream(aGrade);
     int grd;
     while(gradeStream >> grd){
         addGrade(grd);
     }
-    //setCount(); -- REMOVED
-
-    //No Clue If This Is Right
 }
 
 //getters
@@ -77,7 +76,8 @@ void Student::setPhone(long thePhone){
     phoneNumber = thePhone;
 }
 
-//COPY PASTA BE AWARE
+//setCount (copied from README)
+//function will set the count of grades currently in object
 void Student::setCount(){
     //set the counter to the number of grades in the string
     std::istringstream iss(grades);
@@ -89,12 +89,12 @@ void Student::setCount(){
     }
     count = counter;
 }
-//END OF COPY PASTA
 
 //Additional Functions
-//MORE COPY PASTA
+
+//getGrade (copied from README)
+//function will return the grade at given index of grades
 int Student::getGrade(int location){
-    //return the string value of the grade at the given location
     std::istringstream iss(grades);
     std::string value;
 
@@ -106,9 +106,9 @@ int Student::getGrade(int location){
     }
     return -1;
 }
-//END OF COPY PASTA
 
-//CURRENTLY Stacks grades ontop of any existing grades.
+
+//function will replace data in existing Student object
 void Student::setStudent(int aSID, std::string aFName, std::string aLName, std::string anAddress, long aPhone, std::string aGrade){
     setSID(aSID);
     setFName(aFName);
@@ -121,45 +121,52 @@ void Student::setStudent(int aSID, std::string aFName, std::string aLName, std::
     while(gradeStream >> grd){
         addGrade(grd);
     }
-    //setCount();
 }
 
+//function will display student info in formatted view
 void Student::displayStudent(){
-    //line of asterisks
+
+    //line of 34 *'s
     std::cout << setfill('*') << setw(34) << '*' << std::endl;
+
     //Rows of student info (note: setfill(' ') needed to replace the fill of '*'s)
-    std::cout << std::left << setfill(' ') << setw(13) << "Student ID" << ":"
-         << std::right << setw(20) << sid << std::endl;
-    std::cout << std::left << setw(13) << "First Name" << ":"
-         << std::right << setw(20) << fName << std::endl;
-    std::cout << std::left << setw(13) << "Last Name" << ":"
-         << std::right << setw(20) << lName << std::endl;
-    std::cout << std::left << setw(13) << "Address" << ":"
+    //left aligned data type in a field of 13 characters
+    //separated by ":"
+    //right aligned data in field of 20 characters
+    std::cout << left << setfill(' ') << setw(13) << "Student ID" << ":"
+         << right << setw(20) << sid << std::endl;
+    std::cout << left << setw(13) << "First Name" << ":"
+         << right << setw(20) << fName << std::endl;
+    std::cout << left << setw(13) << "Last Name" << ":"
+         << right << setw(20) << lName << std::endl;
+    std::cout << left << setw(13) << "Address" << ":"
          << std::right << setw(20) << address << std::endl;
     std::cout << std::left << setw(13) << "Phone Number" << ":"
          << std::right << setw(20) << phoneNumber << std::endl;
     std::cout << setfill('*') << setw(34) << '*' << std::endl;
 }
 
+//function will add a new grade to current grades
 void Student::addGrade(int theGrade){
+
+    //validates if theGrade is valid grade format
     if((theGrade <= 100) & (theGrade >= 0)){
+
+        //converts int to string
         std::string gradeStr = std::to_string(theGrade);
 
+        //adds grade to grades
         grades += " " + gradeStr;
         setCount();
 
     }
 }
 
-/*
-std::string convertLetterGrade(int)
-Given a grade value (parameter), the method will return the corresponding letter grade.
-Use our course grading scale from the syllabus to obtain the letter grade. (Change: If grade is between 0 and 60, then the letter grade is an 'F')
-The integer value should be considered a valid grade.
-Must be implemented using a switch statement.
-*/
+//function will convert number grades to letter grade based on grading scale
 std::string Student::convertLetterGrade(int theGrade){
     std::string letterGrade;
+
+    //sets percentage ranges for grades
     int range;
 
     if(theGrade <= 60){
@@ -199,6 +206,7 @@ std::string Student::convertLetterGrade(int theGrade){
         range = 11;
     }
 
+    //switch case converts different grade percentage ranges to letter grad
     switch(range){
         case 0:
         {
@@ -264,72 +272,53 @@ std::string Student::convertLetterGrade(int theGrade){
     return letterGrade;
 }
 
+//function will give current average grade of all grades, in letter form
 std::string Student::currentLetterGrade(){
     std::istringstream gradeStream(grades);
     int grd;
     int grdSum;
+    //streams grade from string grades into int format, adds int to grdSum
     while(gradeStream >> grd){
         grdSum += grd;
     }
+
+    //takes average of grades (rounds down)
     int grdAvg = grdSum / count;
 
+    //converts average grade to a letter grade
     std::string currGrade = convertLetterGrade(grdAvg);
 
     return currGrade;
 }
 
-/*
-void listGrades()
-The function will print to standard output a table with each of the grades and the cumulative average of grades.
-Must use a for loop, and format the output stream as discussed in class using the formatting functions.
-The table format contains two columns. The first one with three characters and the second one with ten characters. A space is added between them (a total of fourteen characters).
-Grades should be shown as integers.
-The cumulative average must have two decimal digits.
-Example (assuming the grades given above)
-*/
+
+//function will print to standard output a table with each of the grades and the cumulative average of grades.
 void Student::listGrades(){
     int currGrade;
     int gradeSum = 0;
     float cumAvg;
 
+    //outputs 14 *'s
     std::cout << setfill('*') << setw(14) << "*" <<std::endl;
-    std::cout << left << setfill(' ') << setw(3) << "GRD" << " "
-              << right << setw(10) << "Cum.Avg." << std::endl;
+
+    //outputs: "GRD" left aligned. A " ". Then "Cum.Avg." right aligned in a 10 character field
+    std::cout << "GRD" << " " << right << setfill(' ') << setw(10) << "Cum.Avg." << std::endl;
+
+    //outputs 14 *'s
     std::cout << setfill('*') << setw(14) << "*" <<std::endl;
 
     for(int counter = 0; counter < count; counter++){
         currGrade = getGrade(counter);
-        //std::cout << "grade at counter: " << counter << ", is: " << getGrade(counter) << std::endl;
         gradeSum += currGrade;
         cumAvg = float(gradeSum)/(counter+1);
 
+        //sets numbers to output with 2 decimal points
         std::cout << std::fixed << std::setprecision(2);
+
+        //outputs currGrade right aligned in a field of 3 chars followed by a ' '
+        //empty space will be filled with ' '
         std::cout << setfill(' ') << setw(3) << currGrade << " "
-                  << right << setw(10) << cumAvg << std::endl;
+                  << setw(10) << cumAvg << std::endl;
+                  //outputs cumAvg right aligned in a field of 10 characters
     }
 }
-/*
-void Student::displayStudent(){
-    //line of asterisks
-    std::cout << setfill('*') << setw(34) << '*' << std::endl;
-    //Rows of student info (note: setfill(' ') needed to replace the fill of '*'s)
-    std::cout << std::left << setfill(' ') << setw(13) << "Student ID" << ":"
-         << std::right << setw(20) << sid << std::endl;
-    std::cout << std::left << setw(13) << "First Name" << ":"
-         << std::right << setw(20) << fName << std::endl;
-    std::cout << std::left << setw(13) << "Last Name" << ":"
-         << std::right << setw(20) << lName << std::endl;
-    std::cout << std::left << setw(13) << "Address" << ":"
-         << std::right << setw(20) << address << std::endl;
-    std::cout << std::left << setw(13) << "Phone Number" << ":"
-         << std::right << setw(20) << phoneNumber << std::endl;
-    std::cout << setfill('*') << setw(34) << '*' << std::endl;
-}
-*/
-
-
-
-
-
-
-
