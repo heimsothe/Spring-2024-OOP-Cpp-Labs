7a9fb3c75d57f92fe21e2910b00cbd30cc1097f1
slothy49 <73962789+slothy49@users.noreply.github.com>
2024-04-05 04:31:49 +0000

    [Lab06] commit of Address.cpp and .h

--- Lab06/src/Address.cpp
+++ Lab06/src/Address.cpp
@@ -57,6 +57,15 @@ void Address::setZip(const size_t& theZipCode) {
 }
 
 // Friend I/O operators
+
+std::ostream& operator<<(std::ostream& out, const Address& address) {
+    out << std::left << std::setw(13) << "STREET" << ": " << std::setw(21) << address.street << "\n";
+    out << std::left << std::setw(13) << "CITY" << ": " << std::setw(21) << address.city << "\n";
+    out << std::left << std::setw(13) << "STATE" << ": " << std::setw(21) << address.state << "\n";
+    out << std::left << std::setw(13) << "ZIP CODE" << ": " << std::setw(21) << address.zipCode << "\n";
+    return out;
+}
+
 std::istream& operator>>(std::istream& in, Address& address) {
     getline(in, address.street);
     getline(in, address.city);
@@ -74,10 +83,3 @@ std::istream& operator>>(std::istream& in, Address& address) {
     return in;
 }
 
-std::ostream& operator<<(std::ostream& out, const Address& address) {
-    out << std::left << std::setw(13) << "STREET" << ": " << std::setw(21) << address.street << "\n";
-    out << std::left << std::setw(13) << "CITY" << ": " << std::setw(21) << address.city << "\n";
-    out << std::left << std::setw(13) << "STATE" << ": " << std::setw(21) << address.state << "\n";
-    out << std::left << std::setw(13) << "ZIP CODE" << ": " << std::setw(21) << address.zipCode << "\n";
-    return out;
-}
