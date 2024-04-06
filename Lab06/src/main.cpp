f1a2671f080d774dec5be757e977d9ff42fbb6ea
slothy49 <73962789+slothy49@users.noreply.github.com>
2024-04-06 04:47:40 +0000

    [Lab06] commit of main.cpp

---
+++ Lab06/src/main.cpp
@@ -0,0 +1,66 @@
+#include "Person.h"
+#include "Pet.h"
+#include "Address.h"
+#include "Date.h"
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+int main() {
+    Person myPerson;
+    //Pet myPet;
+    int choice;
+
+    while (choice != 5) {
+        std::cout << "1. Enter a person\n";
+        std::cout << "2. Enter a new pet\n";
+        std::cout << "3. Display person\n";
+        std::cout << "4. Delete a pet\n";
+        std::cout << "5. Exit\n";
+        std::cout << "Enter your choice: ";
+        std::cin >> choice;
+        std::cin.ignore();
+
+
+        switch (choice) {
+            case 1: {
+                std::cin >> myPerson;
+                break;
+            }
+            case 2: {
+                //myPerson.addPet();
+                try {
+                    myPerson.addPet();
+                }
+                catch (const char* msg) {
+                    std::cerr << msg << std::endl;
+                }
+                break;
+            }
+            case 3: {
+                std::cout << myPerson;
+                break;
+            }
+            case 4: {
+                //myPerson.deletePet();
+                try {
+                    myPerson.deletePet();
+                }
+                catch (const char* msg) {
+                    std::cerr << msg << std::endl;
+                }
+                break;
+            }
+            case 5: {
+                std::cout << "Exiting\n";
+                break;
+            }
+            default: {
+                std::cout << "Invalid choice. Please try again.\n";
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
