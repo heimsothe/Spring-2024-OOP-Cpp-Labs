36fe9d11f4b3092a9d3456a387f4087ab5da9955
Elijah Heimsoth <elijah.heimsoth@ucdenver.edu>
2024-04-11 16:05:05 -0600

    [Lab07] All tests work. Next commit will be final commit with comments.

--- Lab07/src/main.cpp
+++ Lab07/src/main.cpp
@@ -1,10 +1,10 @@
 #include "Loan.h"
 #include "SimpleLoan.h"
+#include "AmortizedLoan.h"
 #include <iostream>
 #include <iomanip>
 #include <fstream>
 #include <string>
-#include <vector>
 
 int main() {
     // Create an empty simple loan
@@ -15,10 +15,27 @@ int main() {
 
     // Create a simple loan with parameters
     // simpleLoan(float aPrincipal, float aInterestRate, int aLoanLength)
-    SimpleLoan testLoan(1000, 5, 6);
+    SimpleLoan testSimpleLoan(1000, 5, 4);
     std::cout << "Simple Loan:" << std::endl;
-    testLoan.displayLoan();
+    testSimpleLoan.displayLoan();
     std::cout << std::endl;
 
-    // Create an Amortized Loan
+    // Create an empty Amortized Loan
+    AmortizedLoan emptyAmortizedLoan;
+    std::cout << "Empty Amortized Loan:" << std::endl;
+    emptyAmortizedLoan.displayLoan();
+    std::cout << std::endl;
+
+    // Create an Amortized Loan with parameters
+    //amortizedLoan(float aPrincipal, float aInterestRate, int aLoanLength)
+    AmortizedLoan testAmortizedLoan(1000, 5, 5);
+    std::cout << "Amortized Loan:" << std::endl;
+    testAmortizedLoan.displayLoan();
+    std::cout << std::endl;
+
+    // Test saveLoan function
+    testSimpleLoan.saveLoan();
+    testAmortizedLoan.saveLoan();
+
+
 }
