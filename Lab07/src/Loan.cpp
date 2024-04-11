c1943167144062beec71392662ecc3a40b608db0
Elijah Heimsoth <elijah.heimsoth@ucdenver.edu>
2024-04-11 11:28:36 -0600

    [Lab07] First commit of Loan.h and Loan.cpp

---
+++ Lab07/src/Loan.cpp
@@ -0,0 +1,96 @@
+#include "Loan.h"
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+
+// Default constructor
+Loan::Loan() {
+    principal = 0.0f;
+    interestRate = 0.0f;
+    loanLength = 0;
+    loanType = "";
+}
+
+// Parameterized constructor
+Loan::Loan(float aPrincipal, float aInterestRate, int aLoanLength, std::string aLoanType) {
+    principal = aPrincipal;
+    interestRate = aInterestRate;
+    loanLength = aLoanLength;
+    loanType = aLoanType;
+}
+
+// Virtual destructor
+Loan::~Loan() {
+    // Empty
+}
+
+// Getters
+float Loan::getPrincipal() {
+    return principal;
+}
+
+float Loan::getInterestRate() {
+    return interestRate;
+}
+
+int Loan::getLoanLength() {
+    return loanLength;
+}
+
+std::string Loan::getLoanType() {
+    return loanType;
+}
+
+// Setters
+void Loan::setPrincipal(float thePrincipal) {
+    principal = thePrincipal;
+}
+
+void Loan::setInterestRate(float theInterestRate) {
+    interestRate = theInterestRate;
+}
+
+void Loan::setLoanLength(int theLoanLength) {
+    loanLength = theLoanLength;
+}
+
+void Loan::setLoanType(std::string theLoanType) {
+    loanType = theLoanType;
+}
+
+// displayLoan: Outputs the loan information to the console
+void Loan::displayLoan() {
+    std::cout << "Loan Overview" << std::endl;
+    std::cout << std::string(32, '=') << std::endl;
+    std::cout << std::setw(18) << std::left << "Loan Type:" << getLoanType() << std::endl;
+    std::cout << std::setw(18) << std::left << "Principal:" << getPrincipal() << std::endl;
+    std::cout << std::setw(18) << std::left << "Interest Rate:" << getInterestRate() << "%" << std::endl;
+    std::cout << std::setw(18) << std::left << "Length in Years:" << getLoanLength() << std::endl;
+    std::cout << std::setw(18) << std::left << "Monthly Payment:" << monthlyPayment() << std::endl;
+}
+
+
+// saveLoan function implementation
+void Loan::saveLoan() {
+    // Directory and file name
+    const std::string directory = "data/";
+    const std::string filename = "loans.txt";
+
+    // Open file in output mode (overwrites existing file or creates a new one if it doesn't exist)
+    std::ofstream outFile(directory + filename, std::ios::out);
+    
+    // Check if the file is open
+    if (outFile.is_open()) {
+        // Write the loan information to the file
+        outFile << getPrincipal() << " "
+                << getInterestRate() << " "
+                << getLoanLength() << " "
+                << std::endl;
+        
+        // Close the file
+        outFile.close();
+    } else {
+        std::cerr << "Unable to open file for writing." << std::endl;
+    }
+}
---
+++ Lab07/src/Loan.h
@@ -0,0 +1,43 @@
+#ifndef LOAN_H
+#define LOAN_H
+
+#include <string>
+
+class Loan {
+protected:
+    float principal;
+    float interestRate;
+    int loanLength;
+    std::string loanType;
+
+public:
+    // Default constructor
+    Loan();
+
+    // Parameterized constructor
+    Loan(float aPrincipal, float aInterestRate, int aLoanLength, std::string aLoanType);
+
+    // Destructor
+    virtual ~Loan();
+
+    // Getters
+    float getPrincipal();
+    float getInterestRate();
+    int getLoanLength();
+    std::string getLoanType();
+
+    // Setters
+    void setPrincipal(float thePrincipal);
+    void setInterestRate(float theInterestRate);
+    void setLoanLength(int theLoanLength);
+    void setLoanType(std::string theLoanType);
+
+    // Base class functions
+    virtual float monthlyPayment() const = 0; // Pure virtual function
+    void displayLoan();
+
+    // File I/O
+    void saveLoan();
+};
+
+#endif // LOAN_H
